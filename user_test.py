03887ece8b244ea381769d9b8384369ec01c98b9
Victorvick399 <victorvick399@gmail.com>
2019-11-11 10:34:33 +0300

    add a test for Credentials class

--- user_test.py
+++ user_test.py
@@ -1,5 +1,6 @@
 import unittest
 from user import User
+from user import Credentials
 
 class TestUser(unittest.TestCase):
     """
@@ -10,12 +11,14 @@ class TestUser(unittest.TestCase):
         tearDown clears everything after every test has been run
         '''
         User.users_list = []
+        Credentials.cred_list = []
 
     def setUp(self):
         '''
         setUp runs before the test cases.
         '''
         self.new_account = User ("Victor","ndungi12","twitter")
+        self.new_cred = Credentials ("vexus12@gmail.com","0723456789")
 
     def user_init(self):
         '''
@@ -25,6 +28,15 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_account.password,"ndungi12")
         self.assertEqual(self.new_account.site,"twitter")
 
+        self.assertEqual(self.new_cred.gmail,"vexus12@gmail.com")
+        self.assertEqual(self.new_cred.number,"0723456789")
+
+    def test_save_cred(self):
+        '''
+        This test cases checks whether save_cred functions saves credentials.
+        '''
+        self.new_cred.save_cred()
+        self.assertEqual(len(Credentials.cred_list),1)
 
     def test_add_user(self):
         '''
@@ -33,12 +45,30 @@ class TestUser(unittest.TestCase):
         self.new_account.add_account()
         self.assertEqual(len(User.accounts_list),1)
 
-    def test_delete_user(self):
+    def test_delete_account(self):
         '''
-        Test case checks whether an account is account is deleted when
+        Test case checks whether an account is account is deleted when delete_account is called.
         '''
-        self.new_account.delete_account()
+        new_account = User ("Kivian","kivian12","instagram")
+        new_account.add_account()
+        User.delete_account(new_account)
+        self.assertEqual(len(User.accounts_list),1)
 
+    def test_display_account(self):
+        '''
+        This test case checks whether the listed items are the items in the accounts_list.
+        '''
+        self.assertEqual(User.display_account(),User.accounts_list)
+
+    def test_find_account(self):
+        '''
+        This test case checks whether to check the find_account function.
+        '''
+        new_account = User ("Kivian","kivian12","instagram")
+        new_account.add_account()
+        found_acc = User.find_account("kivian12")
+
+        self.assertEqual(found_acc.password,new_account.password)
 
 if __name__ == '__main__':
     unittest.main()
